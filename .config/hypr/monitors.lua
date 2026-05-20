bf11394da37293cae9dc64fe0ed660271cde352e
Daniel Maynard <danielmaynard7@outlook.com>
2026-05-20 11:27:24 +0100

    Monitor configuration

---
+++ .config/hypr/monitors.lua
@@ -0,0 +1,30 @@
+local monitor_profiles = {
+    ["Desktop"] = {
+        output   = "",
+        mode     = "preferred",
+        position = "auto",
+        scale    = "auto",
+    },
+    ["Laptop"] = {
+        output   = "",
+        mode     = "preferred",
+        position = "auto",
+        scale    = "1.33",
+    },
+    ["Generic"] = {
+        output   = "",
+        mode     = "preferred",
+        position = "auto",
+        scale    = "auto",
+    }
+}
+
+for _, monitor in ipairs(hl.get_monitors()) do
+    if monitor.name == "DP-1" then
+        hl.monitor(monitor_profiles["Desktop"])
+    elseif monitor.name == "eDP-1" then
+        hl.monitor(monitor_profiles["Laptop"])
+    else
+        hl.monitor(monitor_profiles["Generic"])
+    end
+end
